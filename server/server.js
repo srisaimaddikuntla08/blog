3c41ecbff26abb4008ad958082c18871ab0cd094
srisai810 <srisaimaddikuntlams@gmail.com>
2025-08-07 18:02:08 +0530

    new added

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import connectDB from './config/db.js'
+
+const app = express()
+const PORT = process.env.PORT || 8108
+
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+
+await connectDB()
+
+app.get("/",(req,res)=>{
+    res.send("server is in work")
+})
+
+
+
+app.listen(PORT,()=>console.log(`server is running on port:${PORT}`));
